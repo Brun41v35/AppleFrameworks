27ecbc01501bef9adf565f52f1d9cac27e107754
Bruno Alves <alves.bruno945@gmail.com>
2023-12-11 19:46:56 -0800

    Remove `ContentView` and add `FrameworkGridView `

---
+++ FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrameworks
+//
+//  Created by Bruno Silva on 2023-12-11.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
