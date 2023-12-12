53ff49169d7b3dc59b14b515c84e9a22d7e7594f
Bruno Alves <alves.bruno945@gmail.com>
2023-12-11 19:39:13 -0800

    Initial commit

---
+++ AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Bruno Silva on 2023-12-11.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
