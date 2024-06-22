ae7f14566822dd9cbae07f7c84f39384351c4294
Bruno Alves <alves.bruno945@gmail.com>
2024-06-22 12:46:33 -0300

    Add `FrameworkTitleView`

--- AppleFrameworks/FrameworkGridView.swift
+++ AppleFrameworks/FrameworkGridView.swift
@@ -2,16 +2,29 @@ import SwiftUI
 
 struct FrameworkGridView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        FrameworkTitleView(name: "Metal", imageName: "metal")
     }
 }
 
 #Preview {
     FrameworkGridView()
+}
+
+struct FrameworkTitleView: View {
+
+    let name: String
+    let imageName: String
+
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+    }
 }
